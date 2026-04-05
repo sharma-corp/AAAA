72b4f2e85b717d5ffbd3f7bab0cd436cc0f41c8d
Shaivee Sharma <shaiveesharme@Shaivees-MacBook-Air.local>
2026-04-05 19:23:31 +0530

    v 2

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function openLetter() {
   document.getElementById("landing").style.display = "none";
+
   const section = document.getElementById("letterSection");
   section.classList.remove("hidden");
 
   setTimeout(() => {
     document.getElementById("introText").innerText =
-      "I didn’t know how to say this out loud.";
-  }, 1000);
+      "I wasn’t sure how to say this out loud.";
+  }, 800);
 }
